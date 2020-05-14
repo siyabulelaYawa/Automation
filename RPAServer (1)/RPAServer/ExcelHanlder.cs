f6e275f4865a95b839e78321bdb9fa0e79c4422e
siyabulelaYawa <siyabulela.yawa@s4.co.za>
2020-05-14 10:50:43 +0200

    Edited excel

--- RPAServer (1)/RPAServer/ExcelHanlder.cs
+++ RPAServer (1)/RPAServer/ExcelHanlder.cs
@@ -24,6 +24,7 @@ namespace CoreServer
             Guid id = Guid.NewGuid();
 
             global.fileIndex.Add(id.ToString(), excelFile);
+            excelFile.Save();
 
             return id.ToString();
         }
